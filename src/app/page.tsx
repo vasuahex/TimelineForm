932e6f7fc6f595ca22646b04e1e70d4ac3f470e8
vasu <knsrinivasareddy@gmail.com>
2023-10-31 18:51:08 +0530

    changes done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,24 +8,24 @@ import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import FastfoodIcon from '@mui/icons-material/Fastfood';
 import Typography from '@mui/material/Typography';
-
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import SchoolIcon from '@mui/icons-material/School';
 import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
-
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import { handleCase } from '@/redux/features/auth-slice';
 import PersonalDetails from './components/PersonalDetails';
 import EducationDetails from './components/EducationDetails';
 import Skills from './components/Skills';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function CustomizedTimeline() {
   const timelineStyle = {
     transform: 'rotate(-90deg)',
   };
   const dispatch = useDispatch()
-  const { condition } = useSelector((state: RootState) => state.auth)
+  const { condition, formData } = useSelector((state: RootState) => state.auth)
   // console.log(condition, "cond");
 
   // React.useEffect(() => {
@@ -59,7 +59,9 @@ export default function CustomizedTimeline() {
           >
           </TimelineOppositeContent>
           <TimelineSeparator>
-            <div className='flex flex-row justify-center w-full gap-16 cursor-pointer' onClick={() => dispatch(handleCase(1))}>
+            <div className='flex flex-row justify-center w-full gap-16 cursor-pointer' onClick={() => {
+              dispatch(handleCase(1))
+            }}>
               <TimelineConnector sx={timelineStyle} />
               <div>
                 <TimelineDot className={`${condition <= 3 ? "bg-blue-500  shadow-xl" : ""}`}>
@@ -82,7 +84,10 @@ export default function CustomizedTimeline() {
           >
           </TimelineOppositeContent>
           <TimelineSeparator>
-            <div className='flex flex-row justify-center w-full gap-16 cursor-pointer' onClick={() => dispatch(handleCase(2))}>
+            <div className='flex flex-row justify-center w-full gap-16 cursor-pointer' onClick={() => {
+              formData.firstname != "" && formData.lastname != "" && formData.email != "" && formData.mobile != "" && formData.dateofbirth != "" ?
+                dispatch(handleCase(2)) : toast.error("Enter All Details!!")
+            }}>
               <TimelineConnector sx={timelineStyle} />
               <div>
                 <TimelineDot className={`${condition >= 2 ? "bg-red-300  shadow-xl" : ""}`}>
@@ -107,7 +112,10 @@ export default function CustomizedTimeline() {
           </TimelineOppositeContent>
 
           <TimelineSeparator>
-            <div className='flex flex-row justify-center w-full gap-16 cursor-pointer' onClick={() => dispatch(handleCase(3))}>
+            <div className='flex flex-row justify-center w-full gap-16 cursor-pointer' onClick={() => {
+              formData.universityname != "" && formData.coursename != "" && formData.fieldofstudy != "" && formData.gpa != "" ?
+                dispatch(handleCase(3)) : toast.error("Enter All Details!!")
+            }}>
               <div>
                 <TimelineDot className={`${condition === 3 ? "bg-green-400 shadow-xl" : ""}`}>
                   <WorkspacePremiumIcon />
